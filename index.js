ba729b00f32ed69610a62f072888d4c95e504548
sanober15 <sanober@sanober.me>
2017-12-09 05:33:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ function viewCart() {
   if (cart.length <= 0) {
         console.log("Your shopping cart is empty.");
     }
-
+const l = cart.length;
       let cartList = [];
 
       for (let i = 0; i < l; i++) {
